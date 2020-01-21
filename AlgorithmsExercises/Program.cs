7f1d0ae6e0047fb83c91b31eb5e6f76d7c9adb42
Aretomko <44264336+Aretomko@users.noreply.github.com>
2020-01-21 20:50:27 +0100

    Add files via upload

---
+++ AlgorithmsExercises/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AlgorithmsExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arrays = new  int [][]{new int[] { 8, 5, 4, 6, 2, 9, 1 },new int[] { 3, 5, 2, 1, 9, 8, 7 },new int[] { 2, 3, 9, 4, 1, 2, 6 },new int[] { 6, 0, 1, 2, 3, 4, 5 },new int[] { 7, 8, 9, 0, 9, 8, 7 } };
+            foreach (int[] i in arrays)
+            {
+                Stopwatch timer = Stopwatch.StartNew();
+                timer.Start();
+                Sort.IsertionSort(i);
+                timer.Stop();
+                Console.WriteLine(timer.ElapsedMilliseconds);
+                timer.Reset();
+            }
+            foreach (int[] i in arrays)
+            {
+                Stopwatch timer = Stopwatch.StartNew();
+                timer.Start();
+                Sort.SelectionSort(i);
+                timer.Stop();
+                Console.WriteLine(timer.ElapsedMilliseconds);
+                timer.Reset();
+            }
+            foreach (int[] i in arrays)
+            {
+                Stopwatch timer = Stopwatch.StartNew();
+                timer.Start();
+                Sort.MergeSort(i);
+                timer.Stop();
+                Console.WriteLine(timer.ElapsedMilliseconds);
+                timer.Reset();
+            }
+            foreach (int[] i in arrays)
+            {
+                Stopwatch timer = Stopwatch.StartNew();
+                timer.Start();
+                Sort.QuickSort(i);
+                timer.Stop();
+                Console.WriteLine(timer.ElapsedMilliseconds);
+                timer.Reset();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ AlgorithmsExercises/Sort.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsExercises
+{
+    public class Sort
+    {
+        public static int[] IsertionSort(int[] inputArray) 
+        {
+            if (inputArray.Length == 0 || inputArray.Length == 1) return inputArray;
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > inputArray[j])
+                    {
+                        int temp = inputArray[j - 1];
+                        inputArray[j - 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+            return inputArray;
+        }
+        public static int[] SelectionSort(int[] inputArray) 
+        {
+            if (inputArray.Length == 0 || inputArray.Length == 0) return inputArray;
+
+            int temp, smallest;
+            int n = inputArray.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                smallest = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (inputArray[j] < inputArray[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                temp = inputArray[smallest];
+                inputArray[smallest] = inputArray[i];
+                inputArray[i] = temp;
+            }
+            return inputArray;
+        }
+        public static int[] MergeSort(int[] array)
+        {
+            int[] left;
+            int[] right;
+            int[] result = new int[array.Length];
+            //As this is a recursive algorithm, we need to have a base case to 
+            //avoid an infinite recursion and therfore a stackoverflow
+            if (array.Length <= 1)
+                return array;
+            // The exact midpoint of our array  
+            int midPoint = array.Length / 2;
+            //Will represent our 'left' array
+            left = new int[midPoint];
+
+            //if array has an even number of elements, the left and right array will have the same number of 
+            //elements
+            if (array.Length % 2 == 0)
+                right = new int[midPoint];
+            //if array has an odd number of elements, the right array will have one more element than left
+            else
+                right = new int[midPoint + 1];
+            //populate left array
+            for (int i = 0; i < midPoint; i++)
+                left[i] = array[i];   
+            int x = 0;
+            for (int i = midPoint; i < array.Length; i++)
+            {
+                right[x] = array[i];
+                x++;
+            }
+            left = MergeSort(left);
+            right = MergeSort(right);
+            result = Merge(left, right);
+            return result;
+        }
+        private static int[] Merge(int[] left, int[] right)
+        {
+            int resultLength = right.Length + left.Length;
+            int[] result = new int[resultLength];
+            
+            int indexLeft = 0, indexRight = 0, indexResult = 0;
+            while (indexLeft < left.Length || indexRight < right.Length)
+            {
+                if (indexLeft < left.Length && indexRight < right.Length)
+                {
+                    //If item on left array is less than item on right array, add that item to the result array 
+                    if (left[indexLeft] <= right[indexRight])
+                    {
+                        result[indexResult] = left[indexLeft];
+                        indexLeft++;
+                        indexResult++;
+                    }
+                    // else the item in the right array wll be added to the results array
+                    else
+                    {
+                        result[indexResult] = right[indexRight];
+                        indexRight++;
+                        indexResult++;
+                    }
+                }
+                //if only the left array still has elements, add all its items to the results array
+                else if (indexLeft < left.Length)
+                {
+                    result[indexResult] = left[indexLeft];
+                    indexLeft++;
+                    indexResult++;
+                }
+                //if only the right array still has elements, add all its items to the results array
+                else if (indexRight < right.Length)
+                {
+                    result[indexResult] = right[indexRight];
+                    indexRight++;
+                    indexResult++;
+                }
+            }
+            return result;
+        }
+        public static int[] QuickSort(int[] arr, int start, int end)
+        {
+            int i;
+            if (start < end)
+            {
+                i = Partition(arr, start, end);
+
+                QuickSort(arr, start, i - 1);
+                QuickSort(arr, i + 1, end);
+            }
+            return arr;
+        }
+
+        private static int Partition(int[] arr, int start, int end)
+        {
+            int temp;
+            int p = arr[end];
+            int i = start - 1;
+
+            for (int j = start; j <= end - 1; j++)
+            {
+                if (arr[j] <= p)
+                {
+                    i++;
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+            temp = arr[i + 1];
+            arr[i + 1] = arr[end];
+            arr[end] = temp;
+            return i + 1;
+        }
+    }
+}
